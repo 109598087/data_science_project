a0bc1fc878085be8d43be3dc76b69bc5bc70332f
109598087 <t109598087@ntut.org.tw>
2021-06-14 12:52:10 +0800

    get_final_same_company_list

--- femaie.py
+++ femaie.py
@@ -1,31 +1,34 @@
 import pandas as pd
 
-years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016']
-# read data
-table = pd.read_csv(years[0] + '.csv')
+years = ['2010', '2012', '2013', '2014', '2015', '2016']
 
-# 缺失值 -> drop row
-table.dropna(inplace=True)
+for year in years:
+    # read data
+    table = pd.read_csv(year + '.csv')
 
-# change dtype
-# print(table_2010.columns)
-convert_dict = {'女董事': int,
-                '男董事': int,
-                'ROE': float,
-                'ROA': float,
-                "Tobin'sQ": float,
-                }
-for column in table.columns:
-    if '人數' in column:
-        convert_dict[column] = int
-    elif '比例' in column:
-        convert_dict[column] = float
-    elif '年資' in column:
-        convert_dict[column] = float
-# print(convert_dict)
+    # 缺失值 -> drop row
+    table.dropna(inplace=True)
 
-table = table.astype(convert_dict)
-# print(table.dtypes)
+    # change dtype
+    # print(table.columns)
+    convert_dict = {'女董事': int,
+                    '男董事': int,
+                    'ROE': float,
+                    'ROA': float,
+                    "Tobin'sQ": float,
+                    }
+    for column in table.columns:
+        if '人數' in column:
+            convert_dict[column] = int
+        elif '比例' in column:
+            convert_dict[column] = float
+        elif '年資' in column:
+            convert_dict[column] = float
+    # print(convert_dict)
+    table = table.astype(convert_dict)
+    # print(table.dtypes)
 
-print(table['女董事'])
-print(table['女董事'].sum())
+    # print(table['女董事'])
+    print(table['女董事'].sum())
+    print(table['女性董事比例'].sum())
+
